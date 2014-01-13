64788ffac48be2aa6f04da53b1f52df93cc48515
Nick Reed <reednj77@gmail.com>
2014-01-13 13:55:55 -0600

    Measurement links/documents - Closes #4

--- app/helpers/measurements_helper.rb
+++ app/helpers/measurements_helper.rb
@@ -1,2 +1,17 @@
 module MeasurementsHelper
+  def measurement_popover_link(measurement)
+    if measurement.link
+      link_to measurement.link, target: '_blank', title: measurement.link, class: 'btn btn-xs btn-primary' do
+        content_tag :i, nil, class: 'fa fa-external-link'
+      end.html_safe
+    end
+  end
+
+  def measurement_popover_document_link(measurement)
+    if doc = measurement.document
+      link_to download_document_path(doc), title: doc.name, class: 'btn btn-xs btn-info' do
+        content_tag :i, nil, class: 'fa fa-file-text'
+      end.html_safe
+    end
+  end
 end
--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -3,4 +3,14 @@ class Measurement < ActiveRecord::Base
   belongs_to :document
 
   validates_presence_of :value, :year
+
+  before_save :clean
+
+  private
+
+  def clean
+    if link && link.blank?
+      self.link = nil
+    end
+  end
 end
