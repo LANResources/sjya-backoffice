6efc5f9c47f37167bdf46d41a6a4ff05d288a6c6
Nick Reed <reednj77@gmail.com>
2014-04-28 15:59:01 -0500

    Default sort activities by activity_date

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -48,11 +48,11 @@ module ApplicationHelper
     ]
   end
 
-  def sortable(column, title=nil)
+  def sortable(column, title = nil, default_direction = 'asc')
     title ||= column.titleize
     icons = { "asc" => "up", "desc" => "down" }
-    icon = column == sort_column ? content_tag(:i, "", class: "fa fa-caret-#{icons[sort_direction]}") : ''
-    direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+    icon = column == sort_column ? content_tag(:i, "", class: "fa fa-caret-#{icons[sort_direction(default_direction)]}") : ''
+    direction = column == sort_column && sort_direction(default_direction) == "asc" ? "desc" : "asc"
     link = link_to title, params.merge(sort: column, direction: direction, page: nil).reject { |k,v| k == "_" }
     [link, icon].join(' ').html_safe
   end
